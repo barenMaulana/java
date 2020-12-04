7bcf4e137acf591d3684f738ebc8daf2e12ee848
BarenMaulana <barenmaulana@gmail.com>
2020-12-05 03:56:59 +0700

    mempelajari method overriding di java

---
+++ oop/overriding/App.java
@@ -0,0 +1,14 @@
+package oop.overriding;
+
+public class App extends Dapur {
+
+    void chef() {
+        System.out.println("who cooked today is baren chef");
+    }
+
+    public static void main(String[] args) {
+        Dapur masak = new Dapur();
+        masak.chef();
+    }
+
+}
---
+++ oop/overriding/Dapur.java
@@ -0,0 +1,7 @@
+package oop.overriding;
+
+public class Dapur {
+    void chef() {
+        System.out.println("who cooked today is didi chef");
+    }
+}
