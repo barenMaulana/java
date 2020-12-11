86e5d56e1682f4d0b8a396b249d9b0ca775927f2
BarenMaulana <barenmaulana@gmail.com>
2020-12-12 06:24:50 +0700

    menambahkan base64

---
+++ classes/base64/App.java
@@ -0,0 +1,18 @@
+package classes.base64;
+
+import java.util.Base64;
+
+public class App {
+    public static void main(String[] args) {
+        String name = "baren maulana";
+        String result1 = Base64.getEncoder().encodeToString(name.getBytes());
+        System.out.println(result1);
+
+        byte[] result2 = Base64.getDecoder().decode(result1);
+        String result3 = new String(result2);
+        System.out.println(result2);
+        System.out.println(result3);
+
+    }
+
+}
