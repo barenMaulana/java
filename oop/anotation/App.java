9bd72c4c595193e3b1cd3a87b7a9e7dc842a6e6c
BarenMaulana <barenmaulana@gmail.com>
2020-12-11 08:47:24 +0700

    menambahkan annotation

---
+++ oop/anotation/App.java
@@ -0,0 +1,8 @@
+package oop.anotation;
+
+@AnotationJava(appName = "anotation", version = 1)
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
