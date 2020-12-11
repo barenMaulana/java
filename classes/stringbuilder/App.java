1e8b14b1d722fad47376dbbf9f1811a4d860a85c
BarenMaulana <barenmaulana@gmail.com>
2020-12-11 20:13:53 +0700

    menambahkan string builder

---
+++ classes/stringbuilder/App.java
@@ -0,0 +1,12 @@
+package classes.stringbuilder;
+
+public class App {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("baren");
+        builder.append(" ");
+        builder.append("maulana");
+        builder.toString();
+        System.out.println(builder);
+    }
+}
