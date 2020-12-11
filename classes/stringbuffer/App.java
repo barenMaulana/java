6e35c7211b99f5e5702d1fa6248d5ff3eeab15a2
BarenMaulana <barenmaulana@gmail.com>
2020-12-12 04:52:45 +0700

    menambahkan string buffer

---
+++ classes/stringbuffer/App.java
@@ -0,0 +1,11 @@
+package classes.stringbuffer;
+
+public class App {
+    public static void main(String[] args) {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("baren");
+        buffer.append(" ");
+        buffer.append("maulana");
+        System.out.println(buffer.toString());
+    }
+}
