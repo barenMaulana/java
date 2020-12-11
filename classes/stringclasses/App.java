4aeb3f501076edda2e6b5e1edcea5263ea39b72d
BarenMaulana <barenmaulana@gmail.com>
2020-12-11 20:01:02 +0700

    mencoba beberapa java string classes

---
+++ classes/stringclasses/App.java
@@ -0,0 +1,13 @@
+package classes.stringclasses;
+
+public class App {
+    public static void main(String[] args) {
+        String name = "baren maulana";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.length());
+        System.out.println(name.substring(3));
+        System.out.println(name.trim());
+        System.out.println(name.charAt(1));
+    }
+}
